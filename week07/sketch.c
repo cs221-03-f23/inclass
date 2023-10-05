2d7480ef527c29925517af52e7f77b28653a55f6
Phil Peterson <phpeterson@usfca.edu>
2023-10-05 14:25:23 -0700

    10/5 code

---
+++ week07/sketch.c
@@ -0,0 +1,20 @@
+// C useful stuff
+
+// might be useful for finding best score for X or O
+MIN(a, b)
+MAX(a, b)
+
+// ternary statement
+int x = a < b ? 1 : 0;
+
+// equiv
+if (a < b) {
+    x = 1;
+} else {
+    x = 0;
+}
+
+// getting user input
+int r;
+int c;
+fscanf(stdin, "%d %d\n", &r, &c)
